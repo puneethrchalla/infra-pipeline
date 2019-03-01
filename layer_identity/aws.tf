26e83c19243c7bf74d5c17a53a54f3ce530d609c
Puneeth Reddy Challa <puneeth.reddy@reancloud.com>
2019-03-01 05:59:34 -0500

    remove assume role residue

--- layer_identity/aws.tf
+++ layer_identity/aws.tf
@@ -1,9 +1,4 @@
 provider "aws" {
   max_retries = "${var.max_retries}"
   region      = "${var.aws_region}"
-
-  assume_role {
-    role_arn     = "arn:aws:iam::${lookup(var.roles, var.environment)}:role/Jenkins-Admin"
-    session_name = "DPP-Session"
-  }
 }
